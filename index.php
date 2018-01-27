aa15b893666201e97338d679fb46ca64e4c22b55
Lilanka Ravinath <lilankaravinath@outlook.com>
2018-01-27 11:29:34 +0530

    Change view of login page and add some css classes to styles.css

--- index.php
+++ index.php
@@ -10,10 +10,34 @@
   <title>TronsTech</title>
 </head>
 <body>
-<div>
-  <div>
-    <div>
+<div class="container-page">
+  <div class="container-login">
+    <div class="div-form">
+      <form class="form-login">
+        <span class="form-login-title">Account Login</span>
+        <span class="txt1">Username</span>
+        <div class="div-input">
+          <input class="input" type="text">
+        </div>
+        <span class="txt1">Password</span>
+        <div class="div-input">
+          <span class="btn-show-pass">
+							<i class="fa fa-eye"></i>
+					</span>
+          <input class="input" type="password">
+        </div>
 
+        <div>
+          <div>
+            <input type="checkbox">
+            <lable>Remember me</lable>
+          </div>
+
+          <div>
+            <a href="#">Forgot Password?</a>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </div>
